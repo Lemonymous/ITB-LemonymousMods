4de5cdba37c0dd32e0554a38819de20043ecc189
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-07-22 22:56:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ITB-LemonymousModPack
+ This is a mod pack for Into the Breach by Lemonymous @ https://github.com/Lemonymous, adding:
+
+# How to use
+ Unzip the folder and drop it into your game's mods folder.
+
+ Correct folder structure should look like this: `../Into the Breach/mods/ITB-LemonymousModPack-master`
+
+ You can rename the folder if you want.
---
+++ scripts/init.lua
@@ -0,0 +1,43 @@
+
+local mod =  {
+	id = "lmn_mod_pack",
+	name = "Lemonymous' Modpack",
+	version = "0.2.0",
+	modApiVersion = "2.6.0",
+	--icon = "img/icons/mod_icon.png",
+	description = "A Collection of mods made by Lemonymous",
+	requirements = {},
+	submodFolders = {"mods/"}
+}
+
+local components = {
+	"scripts/LApi/LApi",
+}
+
+function mod:init()
+
+	-- initialize components
+	for _, subpath in ipairs(components) do
+		local name = self.resourcePath .. subpath
+		local comp = require(name)
+		
+		if type(comp) == 'table' and comp.init then
+			comp:init()
+		end
+	end
+end
+
+function mod:load(options, version)
+
+	-- load components
+	for _, subpath in ipairs(components) do
+		local name = self.resourcePath .. subpath
+		local comp = require(name)
+		
+		if type(comp) == 'table' and comp.load then
+			comp:load(self, options, version)
+		end
+	end
+end
+
+return mod
