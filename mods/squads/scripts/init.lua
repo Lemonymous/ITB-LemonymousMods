fb484ce39e5a27b1725df45a263df14848454103
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-07-23 22:15:15 +0200

    Added submod for squad mods

---
+++ mods/squads/scripts/init.lua
@@ -0,0 +1,11 @@
+
+return {
+	id = "lmn_squads",
+	name = "Squads",
+	modApiVersion = "2.6.0",
+	description = "Squads by Lemonymous.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	init = function() end,
+	load = function() end
+}
