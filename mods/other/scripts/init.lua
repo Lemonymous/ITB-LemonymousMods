61517947205806cb1cc7836a946f896f0324b44c
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-07-23 22:15:24 +0200

    Added submod for "other" mods

---
+++ mods/other/scripts/init.lua
@@ -0,0 +1,11 @@
+
+return {
+	id = "lmn_other",
+	name = "Other",
+	modApiVersion = "2.6.0",
+	description = "Other mods by Lemonymous.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	init = function() end,
+	load = function() end
+}
