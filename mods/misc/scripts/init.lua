f39277e659565453f4f6b20537063372806b7c09
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-07-26 17:07:38 +0200

    Added mods/scripts

---
+++ mods/misc/scripts/init.lua
@@ -0,0 +1,11 @@
+
+return {
+	id = "lmn_misc",
+	name = "Misc",
+	modApiVersion = "2.6.0",
+	description = "Miscellaneous mods by Lemonymous.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	init = function() end,
+	load = function() end
+}
