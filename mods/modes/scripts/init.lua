220a1c3442468672392666391a9eff86acd744e2
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-07-26 17:17:26 +0200

    Added mods/modes

---
+++ mods/modes/scripts/init.lua
@@ -0,0 +1,11 @@
+
+return {
+	id = "lmn_modes",
+	name = "Modes",
+	modApiVersion = "2.6.0",
+	description = "Modes by Lemonymous.",
+	icon = "scripts/icon.png",
+	submodFolders = {"mods/"},
+	init = function() end,
+	load = function() end
+}
