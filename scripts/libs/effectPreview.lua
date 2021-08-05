002424b9e6e146cf31802992e790c82f9c23eb30
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-05 19:55:11 +0200

    Updated modpack library effectPreview.lua to a newer version found in RF1995

--- scripts/libs/effectPreview.lua
+++ scripts/libs/effectPreview.lua
@@ -1,6 +1,6 @@
 
 --------------------------------------------------------
--- Effect Preview v.1.1 - code library
+-- Effect Preview v1.2 - code library
 --------------------------------------------------------
 -- provides functions for previewing
 -- charge, leap, teleport, move,
@@ -64,121 +64,170 @@
 
 local this = {}
 
--- asserts a Point is valid.
-local function AssertPoint(p)
-	local size = Board:GetSize()
-	assert(
-		p            and
-		p.x >= 0     and
-		p.y >= 0     and
-		p.x < size.x and
-		p.y < size.y
-	)
-end
-
 -- move all pawns away from a tile.
 -- use RewindTile to revert changes.
-function this.ClearTile(effect, p1)
+function this:ClearTile(effect, tile)
+	assert(type(effect) == 'userdata')
+	assert(type(tile) == 'userdata')
+	assert(type(tile.x) == 'number')
+	assert(type(tile.y) == 'number')
+	
 	effect:AddScript([[
 		lmn_effect_preview_displaced = {};
-		local point = Point(]].. p1.x ..",".. p1.y ..[[);
-		local pawn = Board:GetPawn(point);
+		local tile = ]].. tile:GetString() ..[[;
+		local pawn = Board:GetPawn(tile);
 		while pawn do
 			pawn:SetSpace(Point(-1, -1));
 			table.insert(lmn_effect_preview_displaced, pawn:GetId());
-			pawn = Board:GetPawn(point);
+			pawn = Board:GetPawn(tile);
 		end;
 	]])
 end
 
 -- move all pawns away from a tile, except pawn with id == 'id'
 -- use RewindTile to revert changes.
-function this.FilterTile(effect, p1, id)
+function this:FilterTile(effect, tile, id)
+	assert(type(effect) == 'userdata')
+	assert(type(tile) == 'userdata')
+	assert(type(tile.x) == 'number')
+	assert(type(tile.y) == 'number')
+	assert(type(id) == 'number')
+	
 	effect:AddScript([[
 		lmn_effect_preview_displaced = {};
-		local point = Point(]].. p1.x ..",".. p1.y ..[[);
-		local pawn = Board:GetPawn(point);
+		local tile = ]].. tile:GetString() ..[[;
+		local pawn = Board:GetPawn(tile);
 		while pawn and pawn:GetId() ~= ]].. id ..[[ do
 			pawn:SetSpace(Point(-1, -1));
 			table.insert(lmn_effect_preview_displaced, pawn:GetId());
-			pawn = Board:GetPawn(point);
+			pawn = Board:GetPawn(tile);
 		end;
 	]])
 end
 
 -- moves all displaced pawns back to it's tile.
 -- used after ClearTile or FilterTile
-function this.RewindTile(effect, p1)
+function this:RewindTile(effect, tile)
+	assert(type(effect) == 'userdata')
+	assert(type(tile) == 'userdata')
+	assert(type(tile.x) == 'number')
+	assert(type(tile.y) == 'number')
+	
 	effect:AddScript([[
 		for _, id in ipairs(lmn_effect_preview_displaced) do
-			Board:GetPawn(id):SetSpace(Point(]].. p1.x ..",".. p1.y ..[[));
+			Board:GetPawn(id):SetSpace(]].. tile:GetString() ..[[);
 		end;
 	]])
 end
 
--- previews damage.
-function this.AddDamage(effect, spaceDamage)
-	AssertPoint(spaceDamage.loc)
+function this:SaveTile(effect, tile)
+	assert(type(effect) == 'userdata')
+	assert(type(tile) == 'userdata')
+	assert(type(tile.x) == 'number')
+	assert(type(tile.y) == 'number')
 	
-	this.ClearTile(effect, spaceDamage.loc)
+	effect:AddScript("_G['".. self.tileState .."']:Save(".. tile:GetString() ..")")
+end
+
+function this:RestoreTile(effect, tile)
+	effect:AddScript("_G['".. self.tileState .."']:Restore(".. tile:GetString() ..")")
+end
+
+-- previews damage.
+function this:AddDamage(effect, spaceDamage)
+	assert(type(effect) == 'userdata')
+	assert(type(spaceDamage) == 'userdata')
+	assert(type(spaceDamage.loc) == 'userdata')
+	assert(type(spaceDamage.loc.x) == 'number')
+	assert(type(spaceDamage.loc.y) == 'number')
+	
+	self:ClearTile(effect, spaceDamage.loc)
 	effect:AddDamage(spaceDamage)
-	this.RewindTile(effect, spaceDamage.loc)
+	self:RewindTile(effect, spaceDamage.loc)
 end
 
 -- previews a charge, but does not move any pawns.
-function this.AddCharge(effect, p1, p2, pathing)
-	AssertPoint(p1)
-	AssertPoint(p2)
+function this:AddCharge(effect, p1, p2, pathing)
+	assert(type(effect) == 'userdata')
+	assert(type(p1) == 'userdata')
+	assert(type(p1.x) == 'number')
+	assert(type(p1.y) == 'number')
+	assert(type(p2) == 'userdata')
+	assert(type(p2.x) == 'number')
+	assert(type(p2.y) == 'number')
+	
 	pathing = pathing or PATH_FLYER
 	
-	this.ClearTile(effect, p1)
+	self:ClearTile(effect, p1)
 	effect:AddCharge(Board:GetPath(p1, p2, pathing), NO_DELAY)
-	this.RewindTile(effect, p1)
+	self:RewindTile(effect, p1)
 end
 
 -- previews a leap, but does not move any pawns.
-function this.AddLeap(effect, p1, p2)
-	AssertPoint(p1)
-	AssertPoint(p2)
+function this:AddLeap(effect, p1, p2)
+	assert(type(effect) == 'userdata')
+	assert(type(p1) == 'userdata')
+	assert(type(p1.x) == 'number')
+	assert(type(p1.y) == 'number')
+	assert(type(p2) == 'userdata')
+	assert(type(p2.x) == 'number')
+	assert(type(p2.y) == 'number')
 	
 	local leap = PointList()
 	leap:push_back(p1)
 	leap:push_back(p2)
 	
-	this.ClearTile(effect, p1)
+	self:ClearTile(effect, p1)
 	effect:AddLeap(leap, NO_DELAY)
-	this.RewindTile(effect, p1)
+	self:RewindTile(effect, p1)
 end
 
 -- previews teleport, but does not move any pawns.
-function this.AddTeleport(effect, p1, p2)
-	AssertPoint(p1)
-	AssertPoint(p2)
+function this:AddTeleport(effect, p1, p2)
+	assert(type(effect) == 'userdata')
+	assert(type(p1) == 'userdata')
+	assert(type(p2) == 'userdata')
+	assert(type(p1.x) == 'number')
+	assert(type(p1.y) == 'number')
+	assert(type(p2.x) == 'number')
+	assert(type(p2.y) == 'number')
 	
-	this.ClearTile(effect, p1)
+	self:ClearTile(effect, p1)
 	effect:AddTeleport(p1, p2, NO_DELAY)
-	this.RewindTile(effect, p1)
+	self:RewindTile(effect, p1)
 end
 
 -- previews a move action, but does not move any pawns.
 -- pathing is optional.
-function this.AddMove(effect, pawn, p2, pathing)
-	AssertPoint(p2)
+function this:AddMove(effect, pawn, p2, pathing)
+	assert(type(effect) == 'userdata')
+	assert(type(pawn) == 'userdata')
+	assert(type(p2) == 'userdata')
+	assert(type(p2.x) == 'number')
+	assert(type(p2.y) == 'number')
+	
 	pathing = pathing or pawn:GetPathProf()
 	
 	local p1 = pawn:GetSpace()
 	local move = Board:GetPath(p1, p2, pathing)
 	
-	this.ClearTile(effect, p1)
+	self:ClearTile(effect, p1)
 	effect:AddMove(move, NO_DELAY)
-	this.RewindTile(effect, p1)
+	self:RewindTile(effect, p1)
 end
 
 -- (should probably be in another library)
 -- causes a pawn to leap, while hiding it's preview arc.
-function this.AddHiddenLeap(effect, p1, p2, delay)
-	AssertPoint(p1)
-	AssertPoint(p2)
+function this:AddHiddenLeap(effect, p1, p2, delay)
+	assert(type(effect) == 'userdata')
+	assert(type(p1) == 'userdata')
+	assert(type(p1.x) == 'number')
+	assert(type(p1.y) == 'number')
+	assert(type(p2) == 'userdata')
+	assert(type(p2.x) == 'number')
+	assert(type(p2.y) == 'number')
+	
+	assert(type(delay) == 'number')
 	
 	local leap = PointList()
 	leap:push_back(p1)
