8b0b15782387112a4e070ddb21bf5d97ab3ece55
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-06 06:43:44 +0200

    Rewrote modpack library weaponArmed.lua
    Rewrote library to use mod loader Events.
    Removed dependency to modApiExt.
    
    Events:
    - .events.onWeaponArmed - is dispatched when a weapon becomes armed.
    - .events.onWeaponUnarmed - is dispatched when a weapon becomes unarmed.
    
    Methods:
    - .getArmedWeapon() - returns the armed skill or nil
    - .isWeaponArmed(skill) - returns `true` if the specified weapon is armed.
    - .isAnyWeaponArmed() - returns `true` if any weapon is armed.

--- scripts/libs/weaponArmed.lua
+++ scripts/libs/weaponArmed.lua
@@ -1,193 +1,100 @@
 
---------------------------------------------
--- Weapon Armed - code library
---------------------------------------------
--- provides hooks for when a weapon is
--- being armed and unarmed.
---------------------------------------------
---------------------------------------------
+--[[
 
--------------------------------
--- initialization and loading:
---[[---------------------------
+	Weapon Armed v2.0 - code library by Lemonymous
 
-	-- in init.lua
-	local weaponArmed
 
-	-- in init.lua - function init:
-	weaponArmed = require(self.scriptPath .."weaponArmed")
-	weaponArmed:init(self)
+	events:
+		.events.onWeaponArmed
+		.events.onWeaponUnarmed
 
-	-- in init.lua - function load:
-	weaponArmed:load(modApiExt)
-	
-	
-	-- after you have initialized and loaded it,
-	-- you can request it again in your weapons with:
-	local weaponArmed = require(self.scriptPath .."weaponArmed")
-	
-]]-----------------------------
+	methods:
+		.getArmedWeapon
+		.isWeaponArmed
+		.isAnyWeaponArmed
 
-------------------
--- function list:
-------------------
+	examples:
+		local mod = mod_loader.mods[modApi.currentMod]
+		local scriptPath = mod.scriptPath
+		local weaponArmed = require(scriptPath.."libs/weaponArmed")
 
------------------------------------------------------------
--- weaponArmed:Add(weapon, onArmedFunc, onUnarmedFunc)
---[[-------------------------------------------------------
-	calls provided functions when weapon is armed/unarmed.
-	
-	example:
-	
-	weaponArmed:Add(
-		"Prime_Punchmech",
-		function(self, type)
-			LOG("weapon ".. type .." with name ".. self.Name .." armed")
-		end,
-		function(self, type)
-			LOG("weapon ".. type .." with name ".. self.Name .." unarmed")
-		end
-	)
-	
-]]---------------------------------------------------------
+		weaponArmed.events.onWeaponArmed:subscribe(function(skill, pawnId)
+			local pawn = Game:GetPawn(pawnId)
+			LOGF("Pawn %s armed weapon %s",
+				tostring(pawn:GetMechName()),
+				tostring(skill.__Id)
+			)
+		end)
 
------------------------------------------
--- weaponArmed:GetPawn()
---[[-------------------------------------
-	returns the selected pawn.
-	regardless if weapon is armed or not
-]]---------------------------------------
+		weaponArmed.events.onWeaponUnrmed:subscribe(function(skill, pawnId)
+			-- A weapon can be unarmed from exiting to main menu,
+			-- so pawn might not exist.
+			local pawn = Game and Game:GetPawn(pawnId) or nil
+			LOGF("Pawn %s unarmed weapon %s",
+				tostring(pawn and pawn:GetMechName() or nil),
+				tostring(skill.__Id)
+			)
+		end)
 
------------------------------------------------------------
--- weaponArmed:GetCurrent()
---[[-------------------------------------------------------
-	returns weapon currently armed, or nil if none are.
-	
-	example:
-	
-	local skill, skillType = weaponArmed:GetCurrent()
-	if skill then
-		LOG(skill.Name .."is currently armed")
-		LOG(skillType .."is currently armed")
-	else
-		LOG(no weapon is currently armed")
-	end
-	
-]]---------------------------------------------------------
+		local armedWeapon = weaponArmed:getArmedWeapon()
+		LOGF("Current armed weapon is %s", tostring(armedWeapon))
 
------------------------------------------------------------
--- weaponArmed:IsCurrent(weapon)
---[[-------------------------------------------------------
-	returns true if weapon is currently armed,
-	otherwise false
-	
-	example:
-	
-	if weaponArmed:IsCurrent("Prime_Punchmech") then
-		LOG("Prime_Punchmech is currently armed")
-	else
-		LOG("Prime_Punchmech is currently not armed")
-	end
-	
-]]---------------------------------------------------------
+		local isWeaponArmed = weaponArmed:isWeaponArmed(Prime_Punchmech)
+		LOGF("Prime_Punchmech is armed is %s", tostring(isWeaponArmed))
 
-local mod = mod_loader.mods[modApi.currentMod]
-local resourcePath = mod.resourcePath
-local scriptPath = mod.scriptPath
+		local isAnyWeaponArmed = weaponArmed:isAnyWeaponArmed()
+		LOGF("Any weapon is armed is %s", tostring(isAnyWeaponArmed))
 
-local modApiExt = LApi.library:fetch("ITB-ModUtils/modApiExt/modApiExt")
+]]
 
-local this = {
-	prev = {},
-	weapon = {},
-	weapons = {}
+local lib = {
+	events = {
+		onWeaponArmed = Event(),
+		onWeaponUnarmed = Event()
+	}
 }
 
-local function GetArmedWeapon(pawn)
-	if not pawn then return {} end
-	
-	local wID = pawn:GetArmedWeaponId()
-	local weapons = modApiExt.pawn:getWeapons(pawn:GetId())
-	
-	if weapons[wID] then
-		return {type = weapons[wID], tbl = _G[weapons[wID]]}
+local armedWeaponCurrent = nil
+local armedWeaponLast = nil
+local selectedPawnIdCurrent = nil
+local selectedPawnIdLast = nil
+
+function lib:getArmedWeapon()
+	return armedWeaponLast, selectedPawnIdLast
+end
+
+function lib:isWeaponArmed(skill)
+	return armedWeaponLast == skill
+end
+
+function lib:isAnyWeaponArmed()
+	return armedWeaponLast ~= nil
+end
+
+modApi.events.onFrameDrawStart:subscribe(function()
+	if Board then
+		selectedPawnIdCurrent = Board:GetSelectedPawnId()
+		if selectedPawnIdCurrent then
+			armedWeaponCurrent = Board:GetPawn(selectedPawnIdCurrent):GetArmedWeapon()
+			armedWeaponCurrent = armedWeaponCurrent and _G[armedWeaponCurrent] or nil
+		end
+	else
+		selectedPawnIdCurrent = nil
+		armedWeaponCurrent = nil
 	end
-	
-	return {}
-end
 
-function this:Add(weapon, onArmedFunc, onUnarmedFunc)
-	assert(type(weapon) == 'string')
-	assert(_G[weapon])
-	assert(type(_G[weapon].GetSkillEffect) == 'function')
-	assert(not onArmedFunc or type(onArmedFunc) == 'function')
-	assert(not onUnarmedFunc or type(onUnarmedFunc) == 'function')
-	
-	onArmedFunc = onArmedFunc or function() end
-	onUnarmedFunc = onUnarmedFunc or function() end
-	self.weapons[weapon] = self.weapons[weapon] or {}
-	table.insert(
-		self.weapons[weapon],
-		{
-			onArmed = onArmedFunc,
-			onUnarmed = onUnarmedFunc
-		}
-	)
-end
+	if selectedPawnIdCurrent ~= selectedPawnIdLast or armedWeaponCurrent ~= armedWeaponLast then
+		if armedWeaponLast then
+			lib.events.onWeaponUnarmed:dispatch(armedWeaponLast, Game and selectedPawnIdLast or nil)
+		end
 
-function this:GetPawn()
-	return self.selected
-end
-
-function this:GetCurrent()
-	local weapon = GetArmedWeapon(self.selected)
-	
-	return weapon.tbl, weapon.type
-end
-
-function this:IsCurrent(weapon)
-	assert(type(weapon) == 'string')
-	
-	return GetArmedWeapon(self.selected).type == weapon
-end
-
-sdlext.addGameExitedHook(function()
-	this.selected = nil
-end)
-
-sdlext.addFrameDrawnHook(function()
-	this.weapon = GetArmedWeapon(this.selected)
-	
-	if
-		this.prev.tbl						and
-		this.weapon.tbl ~= this.prev.tbl	and
-		this.weapons[this.prev.type]
-	then
-		for _, v in ipairs(this.weapons[this.prev.type]) do
-			v.onUnarmed(this.prev.tbl, this.prev.type)
+		if armedWeaponCurrent then
+			lib.events.onWeaponArmed:dispatch(armedWeaponCurrent, selectedPawnIdCurrent)
 		end
 	end
-	
-	if
-		this.weapon.tbl						and
-		this.weapon.tbl ~= this.prev.tbl	and
-		this.weapons[this.weapon.type]
-	then
-		for _, v in ipairs(this.weapons[this.weapon.type]) do
-			v.onArmed(this.weapon.tbl, this.weapon.type)
-		end
-	end
-	
-	this.prev = this.weapon
-	this.weapon = {}
+
+	armedWeaponLast = armedWeaponCurrent
+	selectedPawnIdLast = selectedPawnIdCurrent
 end)
 
-function this:init() end
-function this:load()
-	modApiExt:addPawnSelectedHook(function(_, pawn) self.selected = pawn end)
-	modApiExt:addPawnDeselectedHook(function() self.selected = nil end)
-	modApi:addTestMechEnteredHook(function() modApi:runLater(function() self.selected = Board:GetPawn(0) or Board:GetPawn(1) or Board:GetPawn(2) end) end)
-	modApi:addTestMechExitedHook(function() self.selected = nil end)
-end
-
-return this
+return lib
