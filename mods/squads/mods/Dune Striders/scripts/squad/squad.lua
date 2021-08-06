31dc71de69233e5090704b6b765c200010fc783d
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-06 10:39:15 +0200

    Dune Striders: Updated squad.lua similar to the other squads.

--- mods/squads/mods/Dune Striders/scripts/squad/squad.lua
+++ mods/squads/mods/Dune Striders/scripts/squad/squad.lua
@@ -1,18 +1,19 @@
 
 local mod = mod_loader.mods[modApi.currentMod]
-local squad = {}
 
-function squad:load()
+modApi.events.onModLoaded:subscribe(function(id)
+	if id ~= mod.id then return end
+
 	modApi:addSquad(
 		{
 			id = "lmn_dune_striders",
 			"Dune Striders",
-			"lmn_ds_Commando", "lmn_ds_Gunslinger", "lmn_ds_Swoop"
+			"lmn_ds_Commando",
+			"lmn_ds_Gunslinger",
+			"lmn_ds_Swoop"
 		},
 		"Dune Striders",
 		"These Mechs uses speed and cunning to outmaneuver their enemies.",
 		mod.resourcePath .. "img/icons/squad_icon.png"
 	)
-end
-
-return squad
+end)
