9b9af8b0df604f103cdc760043bc3b1014d15d00
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-06 09:55:41 +0200

    Disposal Mechs: Moved mech files to 'squad/'
    Moved files, and initialize them with LApi.scripts

--- mods/squads/mods/Disposal Mechs/scripts/init.lua
+++ mods/squads/mods/Disposal Mechs/scripts/init.lua
@@ -8,16 +8,17 @@ local mod = {
 	requirements = {}
 }
 
+local scripts = {
+	"palette.lua",
+	"squad/",
+}
+
 function mod:metadata()
 	modApi:addGenerationOption("option_dozer", "Dozer Attack", "Alternate Dozer attacks.", {values = {1,2,3}, value = 3, strings = {"Old", "Old+", "New"}})
 end
 
 function mod:init()
-	require(self.scriptPath.."palette")
-	require(self.scriptPath.."mech_chemical")
-	require(self.scriptPath.."mech_dozer")
-	require(self.scriptPath.."mech_stacker")
-	require(self.scriptPath.."squad/squad")
+	LApi.scripts:init(self.scriptPath, scripts)
 end
 
 function mod:load(options, version)
