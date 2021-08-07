f8c9579f03fd43d205d434480f63c918627e8954
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-07 14:56:01 +0200

    Updated modpack lib tutorialTips
    ## Changes
    - Added all methods starting with lower case. Upper case variants still exist for backwards compatibility.
    - Replaced asserts with mod loader asserts for better debugging.
    - Changed save location to profile modcontent.lua when profile specific configuration is selected in mod loader configuration.
    - Added note about library requiring an unique instance for each mod using the library.

--- scripts/libs/tutorialTips.lua
+++ scripts/libs/tutorialTips.lua
@@ -1,18 +1,20 @@
 
 ---------------------------------------------------------------------
--- Tutorial Tips v1.1 - code library
+-- Tutorial Tips v1.2 - code library
 --
 -- by Lemonymous
 ---------------------------------------------------------------------
 -- small helper lib to manage tutorial tips that will only display once per profile.
 -- can be reset, and would likely be done via a mod option.
+--
+-- Note: Each mod using this library must each have their unique instance of it.
 
 local mod = mod_loader.mods[modApi.currentMod]
 local tips = {}
 local cachedTips
 
 sdlext.config(
-	"modcontent.lua",
+	modApi:getCurrentModcontentPath(),
 	function(obj)
 		obj.tutorialTips = obj.tutorialTips or {}
 		obj.tutorialTips[mod.id] = obj.tutorialTips[mod.id] or {}
@@ -23,7 +25,7 @@ sdlext.config(
 -- writes tutorial tips data.
 local function writeData(id, obj)
 	sdlext.config(
-		"modcontent.lua",
+		modApi:getCurrentModcontentPath(),
 		function(readObj)
 			readObj.tutorialTips[mod.id][id] = obj
 			cachedTips = readObj.tutorialTips
@@ -34,25 +36,25 @@ end
 -- reads tutorial tips data.
 local function readData(id)
 	local result = nil
-	
+
 	if cachedTips then
 		result = cachedTips[mod.id][id]
 	else
 		sdlext.config(
-			"modcontent.lua",
+			modApi:getCurrentModcontentPath(),
 			function(readObj)
 				cachedTips = readObj.tutorialTips
 				result = cachedTips[mod.id][id]
 			end
 		)
 	end
-	
+
 	return result
 end
 
-function tips:ResetAll()
+function tips:resetAll()
 	sdlext.config(
-		"modcontent.lua",
+		modApi:getCurrentModcontentPath(),
 		function(obj)
 			obj.tutorialTips = obj.tutorialTips or {}
 			obj.tutorialTips[mod.id] = {}
@@ -61,31 +63,35 @@ function tips:ResetAll()
 	)
 end
 
-function tips:Reset(id)
-	assert(type(id) == 'string')
+function tips:reset(id)
+	Assert.Equals('string', type(id), "Argument #1")
 	writeData(id, nil)
 end
 
-function tips:Add(tip)
-	assert(type(tip) == 'table')
-	assert(type(tip.id) == 'string')
-	assert(type(tip.title) == 'string')
-	assert(type(tip.text) == 'string')
-	
+function tips:add(tip)
+	Assert.Equals('table', type(tip), "Argument #1")
+	Assert.Equals('string', type(tip.id))
+	Assert.Equals('string', type(tip.title))
+	Assert.Equals('string', type(tip.text))
+
 	Global_Texts[mod.id .. tip.id .."_Title"] = tip.title
 	Global_Texts[mod.id .. tip.id .."_Text"] = tip.text
 end
 
-function tips:Trigger(id, loc)
-	assert(type(id) == 'string')
-	assert(type(loc) == 'userdata')
-	assert(type(loc.x) == 'number')
-	assert(type(loc.y) == 'number')
-	
+function tips:trigger(id, loc)
+	Assert.Equals('string', type(id), "Argument #1")
+	Assert.TypePoint(loc, "Argument #2")
+
 	if not readData(id) then
 		Game:AddTip(mod.id .. id, loc)
 		writeData(id, true)
 	end
 end
+
+-- backwards compatibility
+tips.ResetAll = tips.resetAll
+tips.Reset = tips.reset
+tips.Add = tips.add
+tips.Trigger = tips.trigger
 
 return tips
