a11a31e3e3af1d21dc7445e4998448ccb5871712
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-11 22:19:31 +0200

    ITW: Removed commented out code in deathPetals

--- mods/misc/mods/Into The Wild/scripts/deathPetals.lua
+++ mods/misc/mods/Into The Wild/scripts/deathPetals.lua
@@ -4,36 +4,6 @@ local spaceDamageObjects = require(path .."spaceDamageObjects")
 local this = {}
 local modApiExt = LApi.library:fetch("modApiExt/modApiExt", nil, "ITB-ModUtils")
 
--- insert into missionEnd to fire pawnKilledHooks on minor enemies killed instead of retreating.
---[[local pawn = Board:GetPawn(effect.loc)
-local pawnType = pawn:GetType()
-if _G[pawnType].Minor or _G[pawnType].SpawnLimit == false then
-	effect.sScript = string.format("modApiExt_internal.firePawnKilledHooks(GetCurrentMission(), Board:GetPawn(%s))", effect.loc:GetString())
-else
-	effect.sScript = ""
-end]]
-
---[[sdlext.addFrameDrawnHook(function()
-	if
-		not Board				or
-		not GAME				or
-		not GAME.trackedPawns
-	then
-		return
-	end
-	
-	for id, pd in pairs(GAME.trackedPawns) do
-		local pawn = Board:GetPawn(id)
-		
-		if not pd.dead and pawn:GetHealth() == 0 then
-			local petals = _G[pawn:GetType()].lmn_PetalsOnDeath
-			if petals then
-				local loc = pawn:GetSpace()
-				Board:DamageSpace(spaceDamageObjects.Emitter(loc, petals))
-			end
-		end
-	end
-end)]]
 
 function this:load()
 	modApiExt:addPawnKilledHook(function(_, pawn)
