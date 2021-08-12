7d76b9a1fdb94abb6210180189d9b27ded5c413e
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-12 21:23:21 +0200

    BossRush: Modernized init.lua
    Removed hack to display mod icon

--- mods/modes/mods/Boss Rush/scripts/init.lua
+++ mods/modes/mods/Boss Rush/scripts/init.lua
@@ -1,28 +1,18 @@
 
-
-local oldOrderMods = mod_loader.orderMods
-function mod_loader.orderMods(self, options, savedOrder)
-	local ret = oldOrderMods(self, options, savedOrder)
-	
-	local mod = mod_loader.mods["lmn_boss_rush"]
-	mod.icon = mod.resourcePath .."img/mod_icon.png"
-	
-	return ret
-end
-
-local function init(self)
-	spawner = require(self.scriptPath.."spawner")
-end
-
-local function load(self, options, version)
-end
-
-return {
+local mod = {
 	id = "lmn_boss_rush",
 	name = "Boss Rush",
 	description = "Adds bosses to the final mission based on islands completed \n[Easy] 0\n[Normal] 0-1\n[Hard] 1-3\n[Very Hard] 3-5\n[Impossible] 4-8",
-	version = "1.0.2",
-	requirements = {"kf_ModUtils", "lmn_more_bosses"},
-	init = init,
-	load = load,
+	version = "1.1.0",
+	modApiVersion = "2.6.0",
+	icon = "img/mod_icon.png",
+	requirements = { "lmn_more_bosses" },
 }
+
+function mod:init()
+	require(self.scriptPath.."spawner")
+end
+
+function mod:load(options, version) end
+
+return mod
