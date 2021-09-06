49cbbf1a4e77a6787dde511eaafefe41bcfbf10c
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-09-06 20:07:45 +0200

    Remove commented out options generation
    The cutils module does not accept options from mods anymore. Options can now be specified when loading it with `LApi.cutils:init(options)` (see https://github.com/Lemonymous/ITB-LApi/wiki/cutils).

--- scripts/init.lua
+++ scripts/init.lua
@@ -10,27 +10,6 @@ local mod =  {
 }
 
 function mod:metadata()
---[[
-	modApi:addGenerationOption(
-		"cutils_debug",
-		"cutils Debug Methods",
-		"Enables cutils debug methods",
-		{ enabled == false }
-	)
-	modApi:addGenerationOption(
-		"cutils_verbose_init",
-		"cutils Verbose Init",
-		"Additional debug messages when initializing cutils",
-		{ enabled == false }
-	)
-	modApi:addGenerationOption(
-		"cutils_verbose_calls",
-		"cutils Verbose Calls",
-		"Additional debug messages when calling cutils"..
-		"\n\nWarning: Very LOG heavy. Only use to debug cutils related crashes",
-		{ enabled == false }
-	)
---]]
 end
 
 function mod:init(options)
