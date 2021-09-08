28a2c4956853d4b2e7b4425e1d3a66ffd93835c0
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-09-08 21:01:17 +0200

    Add option file for More Bosses

---
+++ mods/enemies/mods/More Bosses/scripts/options.lua
@@ -0,0 +1,77 @@
+
+local bosses = {
+	"Blobber",
+	"Burrower",
+	"Centipede",
+	"Crab",
+	"Digger",
+	"Leaper",
+	"Scarab",
+}
+
+local function add_element(item, list)
+	if not list_contains(list, item) then
+		list[#list + 1] = item
+	end
+end
+
+local function initializeOptions()
+	local options = {
+		{
+			id = "final",
+			name = "Final Island",
+			desc = "Adds all enabled bosses as possible final island bosses",
+			enabled = false
+		}
+	}
+
+	for _, boss in ipairs(bosses) do
+		options[#options + 1] = {
+			id = boss,
+			name = boss,
+			desc = string.format("Adds %s Leader as a possible island boss", boss),
+			enabled = true,
+		}
+	end
+
+	stablesort(
+		options,
+		function(a, b)
+			return alphanum(a.id, b.id)
+		end
+	)
+
+	for _, option in ipairs(options) do
+		modApi:addGenerationOption(
+			option.id,
+			option.name,
+			option.desc,
+			{ enabled = option.enabled }
+		)
+	end
+end
+
+local function load(self, options)
+	for _, boss in ipairs(bosses) do
+		local mission = _G[string.format("Mission_%sBoss", boss)]
+
+		if mission and options[boss].enabled then
+			add_element(mission, Corp_Default.Bosses)
+
+			if options.final.enabled then
+				add_element(boss, Mission_Final.BossList)
+				add_element(boss, Mission_Final_Cave.BossList)
+			end
+		else
+			remove_element(mission, Corp_Default.Bosses)
+			remove_element(boss, Mission_Final.BossList)
+			remove_element(boss, Mission_Final_Cave.BossList)
+		end
+	end
+end
+
+initializeOptions()
+
+return {
+	load = load
+}
