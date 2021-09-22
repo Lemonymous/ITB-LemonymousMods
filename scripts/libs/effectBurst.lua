3caf5aff27f6af658f590ddeb20bbfdee2bf5d39
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-09-22 23:09:36 +0200

    EffectBurst 2.1: Change to only alter terrain when strictly necessary
    Emitters cannot be created on water or ice, so it is necessary to temporarily change the terrain before creating an emitter on these terrain types. For all other terrain types, it is safer to not alter it, to prevent unforeseen consequences.

--- scripts/libs/effectBurst.lua
+++ scripts/libs/effectBurst.lua
@@ -1,6 +1,6 @@
 
 --------------------------------------
--- Effect Burst v2.0 - helper library
+-- Effect Burst v2.1 - helper library
 --
 -- by Lemonymous
 --------------------------------------
@@ -19,12 +19,21 @@ local function add(effect, loc, emitter, dir)
 	index_terrain = index_terrain or globals:new()
 	index_hp = index_hp or globals:new()
 
-	effect:AddScript(string.format("globals[%s] = Board:GetTerrain(%s)", index_terrain, loc:GetString()))
-	effect:AddScript(string.format("globals[%s] = Board:GetHealth(%s)", index_hp, loc:GetString()))
-	effect:AddScript(string.format("Board:SetTerrain(%s, TERRAIN_ROAD)", loc:GetString()))
+	local iTerrain = Board:GetTerrain(loc)
+	local setRoadTemporarily = iTerrain == TERRAIN_WATER or iTerrain == TERRAIN_ICE
+
+	if setRoadTemporarily then
+		effect:AddScript(string.format("globals[%s] = Board:GetTerrain(%s)", index_terrain, loc:GetString()))
+		effect:AddScript(string.format("globals[%s] = Board:GetHealth(%s)", index_hp, loc:GetString()))
+		effect:AddScript(string.format("Board:SetTerrain(%s, TERRAIN_ROAD)", loc:GetString()))
+	end
+
 	effect:AddBurst(loc, emitter, dir)
-	effect:AddScript(string.format("Board:SetTerrain(%s, globals[%s])", loc:GetString(), index_terrain))
-	effect:AddScript(string.format("Board:SetHealth(%s, globals[%s])", loc:GetString(), index_hp))
+
+	if setRoadTemporarily then
+		effect:AddScript(string.format("Board:SetTerrain(%s, globals[%s])", loc:GetString(), index_terrain))
+		effect:AddScript(string.format("Board:SetHealth(%s, globals[%s])", loc:GetString(), index_hp))
+	end
 end
 
 return {
