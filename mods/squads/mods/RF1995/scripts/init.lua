7c82a258741caa7102580da9fcf5c474cea7b0a6
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-10-02 10:21:32 +0200

    RF1995: Update init
    Update init to be in line with other squads' init, where all detail code is contained in separate files.
    - Add squad.lua - adds the squad to the mod loader.
    - Fix squad sometimes not being recognized as a squad by the mod loader by rearranging Jeep Mech to the last slot. (Mechs must be ordered as Prime > Brute > Ranged > Science and in alphabetized order with their classes)

--- mods/squads/mods/RF1995/scripts/init.lua
+++ mods/squads/mods/RF1995/scripts/init.lua
@@ -9,91 +9,19 @@ local mod =  {
 }
 
 local scripts = {
-	"libs/hooks",
-	"libs/nonMassiveDeployWarning",
-	"libs/track_items",
-	"libs/track_undo_move",
-	"libs/virtualBoard",
-	"libs/weaponMarks",
+	"libs/",
+	"achievements",
 	"palette",
-	"weapons/weapon_launcher",
-	"weapons/weapon_minelayer",
-	"pawns/mech_helicopter",
-	"pawns/mech_jeep",
-	"pawns/mech_minelayer",
-	"pawns/mech_tank",
+	"weapons/",
+	"pawns/",
 }
 
 function mod:init()
-	assert(LApi, string.format("Mod %s with id '%s' requires 'LApi' in order to function properly", self.name, self.id))
-	
-	-- initialize scripts
-	for _, subpath in ipairs(scripts) do
-		local name = self.scriptPath .. subpath
-		local comp = require(name)
-		
-		if type(comp) == 'table' and comp.init then
-			comp:init()
-		end
-	end
-
-	local mechs = {
-		"lmn_JeepMech",
-		"lmn_HelicopterMech",
-		"lmn_TankMech",
-		"lmn_MinelayerMech"
-	}
-
-	modApi.events.onPostStartGame:subscribe(function()
-		matches = 0
-		for _, mech in ipairs(GAME.additionalSquadData.mechs) do
-			if list_contains(mechs, mech) then
-				matches = matches + 1
-			end
-		end
-
-		if matches == 3 then
-			GAME.additionalSquadData.squad = "RF1995"
-		end
-	end)
+	LApi.scripts:init(self.scriptPath, scripts)
 end
 
 function mod:load(options, version)
-	-- load scripts
-	for _, subpath in ipairs(scripts) do
-		local name = self.scriptPath .. subpath
-		local comp = require(name)
-		
-		if type(comp) == 'table' and comp.load then
-			comp:load(options, version)
-		end
-	end
-	
-	math.randomseed(os.time()); math.random()
-	local rng = math.random(1,4)
-	local list = {}
-	list[(rng + 0) % 4 + 1] = "lmn_JeepMech"
-	list[(rng + 1) % 4 + 1] = "lmn_HelicopterMech"
-	list[(rng + 2) % 4 + 1] = "lmn_TankMech"
-	list[(rng + 3) % 4 + 1] = "lmn_MinelayerMech"
-	
-	modApi:addSquad(
-		{
-			"RF1995",
-			list[1],
-			list[2],
-			list[3],
-			id = "RF1995"
-		},
-		"RF1995",
-		"How this squad got mixed up in the earth-saving business is unknown. "
-		.. "Now where do the Vek hide their flag?"
-		.. "\n\n(4th mech available in Custom and Random squads)",
-		self.resourcePath .. "img/icon.png"
-	)
-	
-	-- add a 4th member of our squad.
-	table.insert(modApi.mod_squads[#modApi.mod_squads], list[4])
+	LApi.scripts:load(self.scriptPath, scripts, self)
 end
 
 return mod
---
+++ mods/squads/mods/RF1995/scripts/pawns/squad.lua
@@ -0,0 +1,38 @@
+
+local mod = mod_loader.mods[modApi.currentMod]
+local mechs = {
+	"lmn_HelicopterMech",
+	"lmn_TankMech",
+	"lmn_MinelayerMech",
+	"lmn_JeepMech",
+}
+
+math.randomseed(os.time())
+math.random() -- first value is useless
+
+local MECH_INDEX_EXTRA = math.random(1,4)
+local mech_extra = mechs[MECH_INDEX_EXTRA]
+
+table.remove(mechs, MECH_INDEX_EXTRA)
+
+modApi.events.onModLoaded:subscribe(function(id)
+	if id ~= mod.id then return end
+
+	modApi:addSquad(
+		{
+			"RF1995",
+			mechs[1],
+			mechs[2],
+			mechs[3],
+			id = "RF1995"
+		},
+		"RF1995",
+		"How this squad got mixed up in the earth-saving business is unknown. "
+		.. "Now where do the Vek hide their flag?"
+		.. "\n\n(4th mech available in Custom and Random squads)",
+		mod.resourcePath.."img/icon.png"
+	)
+
+	-- add a 4th member of our squad.
+	table.insert(modApi.mod_squads[#modApi.mod_squads], mech_extra)
+end)
