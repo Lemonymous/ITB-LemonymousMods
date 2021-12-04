5c196150fbb1d6526bc671aa38ecbb68bc96d510
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-12-04 18:42:50 +0100

    FTL Pilots: Delete replaceRepair

--- mods/pilots/mods/FTL Pilots/scripts/replaceRepair/lib/file_exists.lua
+++
@@ -1,5 +0,0 @@
-
-return function(name)
-    local f = io.open(name, "r")
-    if f then io.close(f) return true else return false end
-end
--- mods/pilots/mods/FTL Pilots/scripts/replaceRepair/replaceRepair.lua
+++
@@ -1,3 +0,0 @@
-
-local path = mod_loader.mods[modApi.currentMod].scriptPath
-return require(path .."replaceRepair/api")
