54ce758f65888322a8f1600d3e29d783e002a2db
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-13 17:59:50 +0100

    Rearrange enemy- and boss lists

--- mods/misc/mods/Into The Wild/scripts/island/bossList.lua
+++ mods/misc/mods/Into The Wild/scripts/island/bossList.lua
@@ -2,8 +2,8 @@
 -- create boss list
 local bossList = modApi.bossList:add("Meridia")
 
+bossList:addBoss("Mission_lmn_SpringseedBoss")
 bossList:addBoss("Mission_lmn_ChiliBoss")
+bossList:addBoss("Mission_lmn_SunflowerBoss")
 bossList:addBoss("Mission_lmn_ChomperBoss")
 bossList:addBoss("Mission_lmn_SequoiaBoss")
-bossList:addBoss("Mission_lmn_SpringseedBoss")
-bossList:addBoss("Mission_lmn_SunflowerBoss")
--- mods/misc/mods/Into The Wild/scripts/island/enemyList.lua
+++ mods/misc/mods/Into The Wild/scripts/island/enemyList.lua
@@ -4,14 +4,14 @@ local enemyList = modApi.enemyList:add("Meridia")
 
 enemyList.categories = {"Core", "Core", "Core", "Unique", "Unique", "Unique"}
 
-enemyList:addEnemy("lmn_Chomper", "Core")
+enemyList:addEnemy("lmn_Sprout", "Core")
 enemyList:addEnemy("lmn_Puffer", "Core")
 enemyList:addEnemy("lmn_Springseed", "Core")
-enemyList:addEnemy("lmn_Sprout", "Core")
+enemyList:addEnemy("lmn_Chomper", "Core")
 enemyList:addEnemy("lmn_Sunflower", "Core")
 
+enemyList:addEnemy("lmn_Chili", "Unique")
 enemyList:addEnemy("lmn_Beanstalker", "Unique")
 enemyList:addEnemy("lmn_Bud", "Unique")
 enemyList:addEnemy("lmn_Cactus", "Unique")
-enemyList:addEnemy("lmn_Chili", "Unique")
 enemyList:addEnemy("lmn_Infuser", "Unique")
