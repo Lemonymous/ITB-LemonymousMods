3fe3444568ed0a5111815e26b1da0bddbfdfb753
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-13 23:53:33 +0100

    Add bots and psions to ITW enemyList

--- mods/misc/mods/Into The Wild/scripts/island/enemyList.lua
+++ mods/misc/mods/Into The Wild/scripts/island/enemyList.lua
@@ -15,3 +15,14 @@ enemyList:addEnemy("lmn_Beanstalker", "Unique")
 enemyList:addEnemy("lmn_Bud", "Unique")
 enemyList:addEnemy("lmn_Cactus", "Unique")
 enemyList:addEnemy("lmn_Infuser", "Unique")
+
+-- add Bots in case the list is used for other islands
+enemyList:addEnemy("Snowtank", "Bots")
+enemyList:addEnemy("Snowart", "Bots")
+enemyList:addEnemy("Snowlaser", "Bots")
+
+-- add Psions in case the list is used for other islands
+enemyList:addEnemy("Jelly_Regen", "Leaders")
+enemyList:addEnemy("Jelly_Health", "Leaders")
+enemyList:addEnemy("Jelly_Armor", "Leaders")
+enemyList:addEnemy("Jelly_Explode", "Leaders")
