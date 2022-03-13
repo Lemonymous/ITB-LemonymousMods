dc6bb4e2038f8a27345cfbc4f0c5e0b4cd54bcdb
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-14 00:26:52 +0100

    Add a More Bosses bossList

---
+++ mods/enemies/mods/More Bosses/scripts/bosses/bossList.lua
@@ -0,0 +1,10 @@
+
+local bossList = modApi.bossList:add("More Bosses")
+
+bossList:addBoss("Mission_ScarabBoss")
+bossList:addBoss("Mission_CrabBoss")
+bossList:addBoss("Mission_DiggerBoss")
+bossList:addBoss("Mission_CentipedeBoss")
+bossList:addBoss("Mission_LeaperBoss")
+bossList:addBoss("Mission_BurrowerBoss")
+bossList:addBoss("Mission_BlobberBoss")
