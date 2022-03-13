2832e7d3c2c0e10a820fb07822176c012e358ffc
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-13 23:57:38 +0100

    ITW v1.3.0

--- mods/misc/mods/Into The Wild/scripts/init.lua
+++ mods/misc/mods/Into The Wild/scripts/init.lua
@@ -2,8 +2,8 @@
 local mod = {
 	id = "lmn_into_the_wild",
 	name = "Into The Wild",
-	version = "1.2.2",
-	modApiVersion = "2.3.0",
+	version = "1.3.0",
+	modApiVersion = "2.6.4",
 	icon = "img/icon.png",
 	requirements = {}
 }
