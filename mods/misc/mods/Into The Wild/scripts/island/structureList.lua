f40db21dd87fc9b7409d9656bd33fee2be6beaed
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-03-13 23:53:49 +0100

    Rearrange ITW structureList

--- mods/misc/mods/Into The Wild/scripts/island/structureList.lua
+++ mods/misc/mods/Into The Wild/scripts/island/structureList.lua
@@ -2,16 +2,16 @@
 local structureList = modApi.structureList:add("Meridia")
 
 structureList:addAssets(
-	"Str_Agroforest",
-	"Str_Depot",
 	"Str_Genomics_Lab",
-	"Str_Geothermal_Plant",
-	"Str_Greenhouse",
+	"Str_Agroforest",
 	"Str_Hotel",
-	"Str_Hydroponic_Farm",
-	"Str_Lightning_Rod",
-	"Str_Observatory",
 	"Str_Outpost",
+	"Str_Observatory",
+	"Str_Greenhouse",
+	"Str_Hydroponic_Farm",
+	"Str_Geothermal_Plant",
+	"Str_Lightning_Rod",
 	"Str_Energy_Reserves",
+	"Str_Depot",
 	"Str_Storehouse"
 )
