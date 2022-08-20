175d301819a2a3783982d29e124dc7af62a7b173
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-08-20 22:57:25 +0200

    LemonymousMods v0.8.5

--- scripts/init.lua
+++ scripts/init.lua
@@ -10,9 +10,9 @@ require(scriptPath().."easyEdit/easyEdit")
 local mod =  {
 	id = "lmn_mods",
 	name = "Lemonymous' Mods",
-	version = "0.8.4",
+	version = "0.8.5",
 	modApiVersion = "2.7.1",
-	gameVersion = "1.2.76b",
+	gameVersion = "1.2.78",
 	icon = "scripts/icon.png",
 	description = "A Collection of mods made by Lemonymous",
 	submodFolders = {"mods/"}
