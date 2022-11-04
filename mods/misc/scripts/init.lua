4d2b13d70718b129ab32d294dc2be9b61cfddff5
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-04 23:37:29 +0100

    Rename modes -> misc

--- mods/misc/scripts/init.lua
+++ mods/misc/scripts/init.lua
@@ -1,9 +1,9 @@
 
 return {
-	id = "lmn_modes",
-	name = "Modes",
+	id = "lmn_misc",
+	name = "Misc",
 	modApiVersion = "2.6.0",
-	description = "Modes by Lemonymous.",
+	description = "Misc mods by Lemonymous.",
 	icon = "scripts/icon.png",
 	submodFolders = {"mods/"},
 	init = function() end,
