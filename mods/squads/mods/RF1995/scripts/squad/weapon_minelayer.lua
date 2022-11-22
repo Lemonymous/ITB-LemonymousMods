3fe24e7e610698648793d33cd7e42867e763c356
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2022-11-22 18:22:57 +0100

    Update RF1995 minelayer weapon
    - Use library `BoardEvents` to track mines and terrain changes
    - Remove unnecessary mini-libraries
    - Fix bug causing Move to throw errors when equipped with upgraded Proximity Mines

--- mods/squads/mods/RF1995/scripts/squad/weapon_minelayer.lua
+++ mods/squads/mods/RF1995/scripts/squad/weapon_minelayer.lua
@@ -1,10 +1,6 @@
 
--- TODO: should be functional for 1.2.
--- However, mines are now technically not dangerous; so warning icon when moving to mines will not show up.
--- Ice will also stay destroyed after undoing move to ice with a mine on it.
--- Mines will remain if terrain changes to water or chasm(unconfirmed)
-local mod = modApi:getCurrentMod()
-local track_items = require(mod.scriptPath.."libs/track_items")
+-- Remaining issues:
+-- Move to mine on unbroken ice -> mine breaks ice -> undo move -> ice remains broken
 
 lmn_Minelayer_Mine = Skill:new{
 	Name = "Proximity Mines",
@@ -40,13 +36,13 @@ local function HasMinesweeper(pawn)
 	if pawn:IsEnemy() or not pawn:IsMech() then
 		return false
 	end
-	
+
 	for _, id in ipairs(extract_table(Board:GetPawns(TEAM_MECH))) do
 		if Board:GetPawn(id):IsWeaponPowered("lmn_Minelayer_Mine_A") then
 			return true
 		end
 	end
-	
+
 	return false
 end
 
@@ -57,7 +53,7 @@ local oldMoveGetSkillEffect = Move.GetSkillEffect
 function Move:GetSkillEffect(p1, p2, ...)
 	origMove = extract_table(oldMoveGetSkillEffect(self, p1, p2, ...).effect)
 	local ret = SkillEffect()
-	
+
 	if IsMinelayer(Pawn) then
 		local damage = SpaceDamage(p1)
 		damage.sItem = "lmn_Minelayer_Item_Mine"
@@ -65,31 +61,31 @@ function Move:GetSkillEffect(p1, p2, ...)
 		damage.sSound = "/impact/generic/grapple"
 		ret:AddDamage(damage)
 	end
-	
+
 	local item_at_destination = Board:GetItem(p2)
-	if item_at_destination then
+	if item_at_destination ~= "" then
 		if HasMinesweeper(Pawn) then
 			lmn_Minelayer_Item_Mine_Dummy = shallow_copy(_G[item_at_destination])
 			lmn_Minelayer_Item_Mine_Dummy.Damage = SpaceDamage()
-			
+
 			local replace_mine = SpaceDamage(p2)
 			replace_mine.sItem = "lmn_Minelayer_Item_Mine_Dummy"
-			
+
 			if item_at_destination == "lmn_Minelayer_Item_Mine" then
 				replace_mine.sImageMark = "combat/icons/rf_icon_minesweeper_glow.png"
 			else
 				replace_mine.sImageMark = "combat/icons/rf_icon_strikeout.png"
 			end
-			
+
 			ret:AddDamage(replace_mine)
 		elseif item_at_destination == "lmn_Minelayer_Item_Mine" then
 			local mark = SpaceDamage(p2)
 			mark.sImageMark = "combat/icons/icon_mine_glow.png"
-			
+
 			ret:AddDamage(mark)
 		end
 	end
-	
+
 	-- reinsert all effect from original SkillEffect
 	for _, v in ipairs(origMove) do
 		ret.effect:push_back(v)
@@ -110,24 +106,24 @@ end
 
 function lmn_Minelayer_Mine_Tip:GetSkillEffect(p1, p2)
 	local ret = SkillEffect()
-	
+
 	local damage = SpaceDamage(p1)
 	damage.sItem = "lmn_Minelayer_Item_Mine"
 	damage.sImageMark = "combat/rf_mark_mine_small.png"
 	ret:AddDamage(damage)
-	
+
 	if self.MineImmunity then
 		damage = SpaceDamage(p2)
 		damage.sItem = "lmn_Minelayer_Item_Mine"
 		damage.sImageMark = "combat/rf_mine_small.png"
 		Board:DamageSpace(damage)
-		
+
 		damage = SpaceDamage(p2)
 		damage.sImageMark = "combat/icons/rf_icon_minesweeper_glow.png"
 		ret:AddDamage(damage)
 	end
 	ret:AddMove(Board:GetPath(p1, p2, Pawn:GetPathProf()), FULL_DELAY)
-	
+
 	return ret
 end
 
@@ -143,22 +139,51 @@ lmn_Minelayer_Item_Mine_Dummy = {} -- modified dynamically
 
 modApi:addWeaponDrop("lmn_Minelayer_Mine")
 
-track_items:addItemRemovedHook(function(mission, loc, removed_item)
+local undoPawnId_thisFrame = nil
+
+modapiext.events.onPawnUndoMove:subscribe(function(mission, pawn)
+	undoPawnId_thisFrame = pawn:GetId()
+end)
+
+BoardEvents.onItemRemoved:subscribe(function(loc, removed_item)
 	if removed_item == "lmn_Minelayer_Item_Mine"  then
-		
+
 		local pawn = Board:GetPawn(loc)
-		local undo_pawn = track_undo_move:GetPawn()
-		
+
 		if pawn then
-			if undo_pawn and pawn:GetId() == undo_pawn:GetId() then
+			if pawn:GetId() == undoPawnId_thisFrame then
 				-- do nothing
 			elseif not HasMinesweeper(pawn) then
 				local mine_damage = SpaceDamage(loc, 3)
 				mine_damage.sSound = "/impact/generic/explosion"
 				mine_damage.sAnimation = "ExploAir1"
-				
+
 				Board:DamageSpace(mine_damage)
 			end
+		end
+	end
+end)
+
+-- modApiExt's events originates from onMissionUpdate
+-- BoardEvents also originates from onMissionUpdate
+-- We want the order to be:
+-- onPawnUndoMove -> onItemRemoved -> reset undo pawn this frame
+--
+-- modApiExt subscribes to onMissionUpdate on modApiExt initialized
+-- BoardEvents subscribes to onMissionUpdate after all mods have initialized
+-- If we subscribe to onMissionUpdate after all mods have initialized as well
+-- this will order it after BoardEvents.
+modApi.events.onModsInitialized:subscribe(function()
+	modApi.events.onMissionUpdate:subscribe(function(mission)
+		undoPawnId_thisFrame = nil
+	end)
+end)
+
+BoardEvents.onTerrainChanged:subscribe(function(p, terrain, terrain_prev)
+	local item = Board:GetItem(p)
+	if item == "lmn_Minelayer_Item_Mine" then
+		if terrain == TERRAIN_HOLE or terrain == TERRAIN_WATER then
+			Board:RemoveItem(p)
 		end
 	end
 end)
